3cedd0681f6eaba43786781a685a685a1115c4ad
KSTEC_dsna <78070883+KstecGit2021@users.noreply.github.com>
2022-09-05 08:54:17 +0900

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,12 +1,12 @@
 from algos.algos import *
 from taipy import Scope, Frequency, Config
 ##############################################################################################################################
-# Creation of the datanodes
+# 데이터노드 생성
 ##############################################################################################################################
-# How to connect to the database
+# 데이터베이스에 연결하는 방법
 path_to_csv = 'data/churn.csv'
 
-# path for csv and file_path for pickle
+# csv의 경로와 피클의 file_path
 initial_dataset = Config.configure_data_node(id="initial_dataset",
                                              path=path_to_csv,
                                              storage_type="csv",
@@ -18,12 +18,12 @@ preprocessed_dataset = Config.configure_data_node(id="preprocessed_dataset",
                                                   cacheable=True,
                                                   validity_period=dt.timedelta(days=1))
 
-# the final datanode that contains the processed data
+# 처리된 데이터를 포함하는 최종 데이터 노드
 train_dataset = Config.configure_data_node(id="train_dataset",
                                            cacheable=True,
                                            validity_period=dt.timedelta(days=1))
 
-# the final datanode that contains the processed data
+# 처리된 데이터를 포함하는 최종 데이터 노드
 trained_model = Config.configure_data_node(id="trained_model",
                                            cacheable=True,
                                            validity_period=dt.timedelta(days=1))
@@ -33,7 +33,7 @@ trained_model_baseline = Config.configure_data_node(id="trained_model_baseline",
                                                     validity_period=dt.timedelta(days=1))
 
 
-# the final datanode that contains the processed data
+# 처리된 데이터를 포함하는 최종 데이터 노드
 test_dataset = Config.configure_data_node(id="test_dataset",
                                           cacheable=True,
                                           validity_period=dt.timedelta(days=1))
@@ -69,44 +69,44 @@ results = Config.configure_data_node(id="results",
 
 
 ##############################################################################################################################
-# Creation of the tasks
+# 작업 생성
 ##############################################################################################################################
 
-# the task will make the link between the input data node 
-# and the output data node while executing the function
+# 작업은 함수를 실행하는 동안 입력 데이터 노드와 출력 데이터 노드 사이의 링크를 만듭니다. 
+# 
 
-# initial_dataset --> preprocess dataset --> preprocessed_dataset
+# 초기 데이터세트 --> 데이터세트 전처리 --> 처리된 데이터세트
 task_preprocess_dataset = Config.configure_task(id="preprocess_dataset",
                                                 input=[initial_dataset,date_cfg],
                                                 function=preprocess_dataset,
                                                 output=preprocessed_dataset)
 
-# preprocessed_dataset --> create train data --> train_dataset, test_dataset
+# 처리된 데이터세트 --> 학습 데이터 생성 --> 학습데이터세트, 테스트데이터세트
 task_create_train_test = Config.configure_task(id="create_train_and_test_data",
                                                input=preprocessed_dataset,
                                                function=create_train_test_data,
                                                output=[train_dataset, test_dataset])
 
 
-# train_dataset --> create train_model data --> trained_model
+# 학습 데이터세트 --> 학습 모델 데이터 생성 --> 학습된 모델
 task_train_model = Config.configure_task(id="train_model",
                                          input=train_dataset,
                                          function=train_model,
                                          output=[trained_model,feature_importance_cfg])
                                    
-# train_dataset --> create train_model data --> trained_model
+# 학습 데이터세트 --> 학습 모델 데이터 생성 --> 학습된 모델
 task_train_model_baseline = Config.configure_task(id="train_model_baseline",
                                                   input=train_dataset,
                                                   function=train_model_baseline,
                                                   output=[trained_model_baseline,feature_importance_cfg])
 
-# test_dataset --> forecast --> forecast_dataset
+# 테스트 데이터세트 --> 예측 --> 예측 데이터 세트
 task_forecast = Config.configure_task(id="predict_the_test_data",
                                       input=[test_dataset, trained_model],
                                       function=forecast,
                                       output=forecast_dataset)
 
-# test_dataset --> forecast --> forecast_dataset
+# 테스트 데이터세트 --> 예측 --> 예측 데이터 세트
 task_forecast_baseline = Config.configure_task(id="predict_of_baseline",
                            input=[test_dataset, trained_model_baseline],
                            function=forecast_baseline,
@@ -135,10 +135,10 @@ task_create_results = Config.configure_task(id="task_create_results",
 
 
 ##############################################################################################################################
-# Creation of the pipeline and the scenario
+# 파이프라인 및 시나리오 생성
 ##############################################################################################################################
 
-# configuration of the pipeline and scenario
+# 파이프라인 및 시나리오 구성
 pipeline_preprocessing = Config.configure_pipeline(id="pipeline_preprocessing", task_configs=[task_preprocess_dataset,
                                                                                               task_create_train_test])
 
@@ -155,42 +155,11 @@ pipeline_baseline = Config.configure_pipeline(id="pipeline_baseline", task_confi
                                                                                     task_create_metrics,
                                                                                     task_create_results])
 
-# the scenario will run the pipelines
+# 시나리오는 파이프라인을 실행합니다.
 scenario_cfg = Config.configure_scenario(id="churn_classification",
                                          pipeline_configs=[pipeline_preprocessing, 
                                                            pipeline_train_baseline, pipeline_train_model,
                                                            pipeline_model,pipeline_baseline],
                                          frequency=Frequency.WEEKLY)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
